054915997b36d560545320b53731d60de42c29b4
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2023-06-14 02:46:47 +0530

    added jwt for auth

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,10 @@
 import pool from "../Database/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+
 
 
 /** POST: http://localhost:8080/api/register 
@@ -24,11 +29,11 @@ export async function register(req, res) {
         if (result[0].length) return res.json("user already existed");
 
         try {
-            
+
             bcrypt.hash(password, 10)
                 .then(hashedPassword => {
-                    const q="INSERT INTO user (`username`, `password`, `email`) VALUES (?,?,?)"
-                     pool.query(q,[username,hashedPassword,email]);
+                    const q = "INSERT INTO user (`username`, `password`, `email`) VALUES (?,?,?)"
+                    pool.query(q, [username, hashedPassword, email]);
                     return res.status(200).json("user successfully created");
                 })
         } catch (error) {
@@ -47,25 +52,33 @@ export async function register(req, res) {
 }
 */
 export async function login(req, res) {
- const {username,password}=req.body;
- try {
-     const q="SELECT * FROM user WHERE username=?";
-     pool.query(q,[username])
-    .then(user=>{
-        bcrypt.compare(password,user[0][0].password)
-        .then(passwordCheck=>{
-           if(!passwordCheck) return res.status(400).send({error:"Dont have password"});
-        })
-        .catch(error =>{
-            return res.status(400).send({error:"Password does not match"});
-        })
-    })
-    .catch(()=>{
-        return res.status(500).send({error:"user does not found"});
-    })
- } catch (error) {
-    return res.status(500).json(error)
- }
+    const { username, password } = req.body;
+    try {
+        const q = "SELECT * FROM user WHERE username=?";
+        pool.query(q, [username])
+            .then(user => {
+                bcrypt.compare(password, user[0][0].password)
+                    .then(passwordCheck => {
+                        if (!passwordCheck) return res.status(400).send({ error: "Dont have password" });
+                        const id=user[0][0].iduser;
+                        
+                        const token=jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:"24h"});
+                        return res.status(200).send({
+                            msg:"Login successfull",
+                            username:username,
+                            token
+                        })
+                    })
+                    .catch(error => {
+                        return res.status(400).send({ error: "Password does not match" });
+                    })
+            })
+            .catch(() => {
+                return res.status(500).send({ error: "user does not found" });
+            })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
 }
 
 /** GET: http://localhost:8080/api/user/example123 */
