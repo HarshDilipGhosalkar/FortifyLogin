cc674afd8ac43de4870c14a01eb26e9a4fabbeed
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2023-06-14 16:37:18 +0530

    login functionality updated

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -24,23 +24,23 @@ export async function register(req, res) {
     const { password, username, email } = req.body;
     try {
 
-        db.query(q, [email, username],(err,result)=>{
+        db.query(q, [email, username], (err, result) => {
             console.log(result);
-         if(result.length !==0) return res.send({msg:"User ALready exists...!"});
-         try {
+            if (result.length !== 0) return res.send({ msg: "User ALready exists...!" });
+            try {
 
-            bcrypt.hash(password, 10)
-                .then(hashedPassword => {
-                    const q = "INSERT INTO user (`username`, `password`, `email`) VALUES (?,?,?)"
-                    db.query(q, [username, hashedPassword, email]);
-                    return res.status(200).json("user successfully created");
-                })
-        } catch (error) {
-            return res.send({error});
-        }
+                bcrypt.hash(password, 10)
+                    .then(hashedPassword => {
+                        const q = "INSERT INTO user (`username`, `password`, `email`) VALUES (?,?,?)"
+                        db.query(q, [username, hashedPassword, email]);
+                        return res.status(200).json("user successfully created");
+                    })
+            } catch (error) {
+                return res.send({ error });
+            }
         });
     } catch (error) {
-        return res.send({error});
+        return res.send({ error });
     }
 }
 
@@ -55,50 +55,48 @@ export async function login(req, res) {
     const { username, password } = req.body;
     try {
         const q = "SELECT * FROM user WHERE username=?";
-        pool.query(q, [username])
-            .then(user => {
-                bcrypt.compare(password, user[0][0].password)
+        db.query(q, [username],(err,data)=>{
+            console.log(data[0].password);
+            if(err) return res.status(400).send({error:"user not found"});
+                  bcrypt.compare(password, data[0].password)
                     .then(passwordCheck => {
                         if (!passwordCheck) return res.status(400).send({ error: "Dont have password" });
-                        const id=user[0][0].iduser;
-                        
-                        const token=jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:"24h"});
+                        const id = data[0].iduser;
+
+                        const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: "24h" });
                         return res.status(200).send({
-                            msg:"Login successfull",
-                            username:username,
+                            msg: "Login successfull",
+                            username: username,
                             token
                         })
                     })
                     .catch(error => {
                         return res.status(400).send({ error: "Password does not match" });
                     })
-            })
-            .catch(() => {
-                return res.status(500).send({ error: "user does not found" });
-            })
+        })
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).send({error})
     }
 }
 
 /** GET: http://localhost:8080/api/user/example123 */
 export async function getUser(req, res) {
-    const {username}=req.params;
+    const { username } = req.params;
     console.log(username);
     try {
-        if(!username) return res.status(501).send({error:"Invalidusername"})
-        const q="SELECT * FROM user WHERE username=?";
-        pool.query(q,[username])
-        .then(result=>{
-           
-           return res.send(result[0]);
-        
-        })
-        .catch(error=>{
-           return res.status(400).send({error:"user not found..."});
-        })
+        if (!username) return res.status(501).send({ error: "Invalidusername" })
+        const q = "SELECT * FROM user WHERE username=?";
+        pool.query(q, [username])
+            .then(result => {
+
+                return res.send(result[0]);
+
+            })
+            .catch(error => {
+                return res.status(400).send({ error: "user not found..." });
+            })
     } catch (error) {
-        return res.status(400).send({error:"cannot find user data"});
+        return res.status(400).send({ error: "cannot find user data" });
     }
 }
 
