06c565ff09f5e61b9879c04a861d035dd1a5fc26
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2023-07-07 18:38:16 +0530

    Zustand setup for state management

---
+++ client/src/store/store.js
@@ -0,0 +1,11 @@
+import create from 'zustand';
+
+export const useAuthStore = create((set) => ({
+    auth : {
+        username : '',
+        email:'',
+        active : false
+    },
+    setUsername : (name) => set((state) => ({ auth : { ...state.auth, username : name }})),
+    setEmail : (email) => set((state) => ({ auth : { ...state.auth, email : email }}))  
+}))
