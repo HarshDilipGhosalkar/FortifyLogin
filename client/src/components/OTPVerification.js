dcf06869105d78cd60a6478f610c0b953519153e
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2023-07-13 23:14:55 +0530

    modified code base of verification

--- client/src/components/OTPVerification.js
+++ client/src/components/OTPVerification.js
@@ -1,50 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from 'formik';
-import { profileValidation } from "../helper/validate";
-import { authenticate } from "../helper/helper";
 import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from "react-router-dom";
-import { generateOTP } from "../helper/helper";
+import { generateOTP, verifyOTP } from "../helper/helper";
+import { useAuthStore } from '../store/store'
 
 export default function OTPVerification({ onclose, visible}) {
+   const{email} =useAuthStore(state=> state.auth)
+   const [OTP,setOTP]=useState("");
     const navigate = useNavigate();
     const handleonClose = (e) => {
         if (e.target.id == "container") onclose();
     }
     
-    const formik = useFormik({
-        initialValues: {
-            OTP: '',
-        },
-        validate: profileValidation,
-        validateOnBlur: false,
-        validateOnChange: false,
-        onSubmit: async values => {
-            console.log(values.email);
-            let authPromise = authenticate(values.email)
-            toast.promise(authPromise, {
-                loading: 'Logging...',
-                success: <b>User Found!</b>,
-                error: <b>Invalid email</b>
-            });
-
-            authPromise.then(res => {
-              generateOTP(values.email)
-                   .then(res => {  
-                        console.log(res)     
-                // onclose();
-                // navigate('/')
-                        
-                                           
-                    })
-                    .catch(error => {
-                        console.log(error)
-                    });
-               
-            })
-            .catch(() => navigate('/'));
-        }
+   
+    async function onSubmit(e){
+        e.preventDefault();
+      try {
+       const{status}= await verifyOTP(OTP)
+       if(status === 201){
+        toast.success('Verify Successfully!')
+         navigate('/reset')
+      } 
+      } catch (error) {
+        return toast.error('Wront OTP! Check email again!')
+      }
+    }
+    
+    async function resendOTP(){
+     generateOTP(email)
+     .then(code => {
+        console.log("otppromisse",code);
+        toast.success("OTP has been send to your email!")
     })
+    .catch(error => {
+        toast.error("Could not Send it!")
+
+    });
+    }
 
     if (!visible) return null;
     return (
@@ -52,12 +45,12 @@ export default function OTPVerification({ onclose, visible}) {
             <Toaster position='top-center' reverseOrder={false}></Toaster>
 
             <div className="bg-white flex flex-col rounded-2xl shadow-lg p-10 sm:max-w-5xl max-w-[90%]">
-                <h1 className="font-bold text-2xl text-[#376549]">Verify OTP</h1>
-                <p className="text-xs mt-4 text-[#477E56]">Enter th OTP.</p>
-                <form className="flex flex-col gap-4" onSubmit={formik.handleSubmit}>
-                    <input className="p-2 w-[100%] mt-8 rounded-[12px] border-2 border-[#AAD468] focus:border-teal-500 focus:outline-none text-xs text-[gray]" {...formik.getFieldProps('OTP')} type="text" placeholder='OTP*' />
-                    <button className="text-[14px] rounded-[12px] w-[100%] bg-[#376549] text-[#CBDFC3] py-[6px] border-2 border-[#AAD468] hover:border-teal-500" type='submit'>Submit</button>
-
+                <h1 className="font-bold text-2xl text-[#376549]">Recovery</h1>
+                <p className="text-xs mt-4 text-[#477E56]">Enter OTP to Recover password.</p>
+                <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+                    <input className="p-2 w-[100%] mt-8 rounded-[12px] border-2 border-[#AAD468] focus:border-teal-500 focus:outline-none text-xs text-[gray]" onChange={(e)=>setOTP(e.target.value)} type="text" placeholder='OTP*' />
+                    <button className="text-[14px] rounded-[12px] w-[100%] bg-[#376549] text-[#CBDFC3] py-[6px] border-2 border-[#AAD468] hover:border-teal-500" type='submit'>Recover</button>
+                    <p className="text-xs mt-4 text-[#477E56] cursor-pointer" onClick={resendOTP}>Cant get OTP.<span className="text-[red] ">Resend?</span></p>
                 </form>
             </div>
         </div>
